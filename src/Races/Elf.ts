08e7b7bf3fbc98add03ce0cf728f0f043b68ff45
brunogsr <brunogsr https://github.com/brunogsr>
2023-09-23 15:24:05 -0300

    req 2

---
+++ src/Races/Elf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private static _createdInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf._createdInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdInstances;
+  }
+}
